b856ea0a35759190b145af2e68d20088fc9a9b7d
Richard Garai <dargzero@gmail.com>
2020-08-28 21:54:40 +0200

    book model

---
+++ konyvmuzeum-svc/model/book.go
@@ -0,0 +1,12 @@
+package model
+
+// Book stores the attributes of a single book
+type Book struct {
+	Author    string
+	Title     string
+	Year      int
+	ISBN      string
+	Publisher string
+	Language  string
+	Pages     int
+}
