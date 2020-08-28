ad3892d7c0064282e08f25a93d046a954564b704
Richard Garai <dargzero@gmail.com>
2020-08-28 23:01:56 +0200

    docker compose mongo connect

--- svc/go.mod
+++ svc/go.mod
@@ -1,3 +1,5 @@
 module okki.hu/konyvmuzeum
 
 go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.0 // indirect
--- svc/main.go
+++ svc/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	fmt.Printf("hai")
+	client, err := mongo.NewClient(
+		options.Client().ApplyURI("mongodb://db:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*30)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	names, err := client.ListDatabaseNames(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, name := range names {
+		fmt.Println(name)
+	}
 }
